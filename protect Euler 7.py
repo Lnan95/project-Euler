9cdd2eb6747493551149cc2874e5bd95f7b9042e
Lin Sinan <464818805@qq.com>
2017-05-21 16:36:04 +0800

    7-8

---
+++ protect Euler 7.py
@@ -0,0 +1,14 @@
+'''
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+What is the 10 001st prime number?
+'''
+
+import numpy as np
+prime = [2,3,5,7,11,13]
+n = 15
+while len(prime) <= 10000:
+    n += 2
+    # num = np.sqrt(n)
+    if all((n%np.array(prime))!=0):
+        prime.append(n)
+print("the 10 001st prime number is {}".format(prime[-1]))
